7c4ab6519455ded822d56e51a9dd1d54eafde2af
nkwtnb <n.watanabe0305@gmail.com>
2022-09-26 00:56:58 +0900

    基本機能の実装完了

--- src/main/java/org/example/Form.java
+++ src/main/java/org/example/Form.java
@@ -3,42 +3,54 @@ package org.example;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.codec.Decoder;
+import org.apache.commons.codec.binary.Base64OutputStream;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.ss.util.CellRangeAddress;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import javax.imageio.ImageIO;
+import java.io.*;
 import java.lang.reflect.Array;
-import java.util.Iterator;
-import java.util.Objects;
+import java.net.URI;
+import java.nio.Buffer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.*;
 
 public class Form {
-    private static final String TEMPLATE_PATH = "./src/main/resources/template_multi.xlsx";
-    private static final String OUT_PATH = "./src/main/resources/out.xlsx";
+//  元ファイルのコピー用suffix（このファイルに対して修正する）
+    private static final String SUFFIX = "_temp.xlsx";
+//  セルごとの項目群のキー / 明細部の項目群のキー
+    private static final String FILED_KEY_CELL = "cell";
+    private static final String FILED_KEY_ROW = "row";
     private static final ObjectMapper mapper = new ObjectMapper();
     private final JsonNode cellNode;
     private final JsonNode rowNode;
     private final Iterator<String> cellNames;
     private final Iterator<String> rowNames;
-    private final Workbook workbook;
+    private final XSSFWorkbook workbook;
     private final Sheet sheet;
-    public Form(String json) throws IOException {
+    private final String filePath;
+    public Form(String srcPath, String json) throws IOException {
         JsonNode _map = mapper.readTree(json);
-        cellNode = _map.get("cell");
-        rowNode = _map.get("row");
+        cellNode = _map.get(FILED_KEY_CELL);
+        rowNode = _map.get(FILED_KEY_ROW);
         // TODO null check
         cellNames = cellNode.fieldNames();
         rowNames = rowNode.fieldNames();
-        workbook = WorkbookFactory.create(new File(TEMPLATE_PATH));
+        filePath = srcPath + SUFFIX;
+        Files.copy(Paths.get(srcPath), Paths.get(filePath), StandardCopyOption.REPLACE_EXISTING);
+        workbook = new XSSFWorkbook(filePath);
         sheet = workbook.getSheetAt(0);
     }
     public void setValues() {
         while(cellNames.hasNext()) {
             var cellName = cellNames.next();
             Name name = workbook.getName(cellName);
-            // TODO nameがNULLの場合はExceptionとするか何もしないか。とりあえず何もしない。
+            // TODO nameがNULLの場合は例外とするか何もしないか。例外としてメッセージを返した方が親切？
             if (Objects.isNull(name)) {
                 continue;
             }
@@ -56,36 +68,46 @@ public class Form {
         while(rowNames.hasNext()) {
             String rowName = rowNames.next();
             Name name = workbook.getName(rowName);
-            // TODO nameがNULLの場合はExceptionとするか何もしないか。とりあえず何もしない。
+            // TODO nameがNULLの場合は例外とするか何もしないか。例外としてメッセージを返した方が親切？
             if (Objects.isNull(name)) {
                 continue;
             }
-            // 対象の名前セルの1件あたりの行数
+            // 対象の名前セルの1件あたりの行数を取得
             CellRangeAddress nameRange = CellRangeAddress.valueOf(name.getRefersToFormula());
             int rowsPerItem = nameRange.getLastRow() - nameRange.getFirstRow() + 1;
-            prepareNextRow(nameRange, rowsPerItem);
-            for (int i=0; i<rowsPerItem; i++) {
-                Row sourceRow = sheet.getRow(nameRange.getFirstRow() + i);
-                Row destinationRow = sheet.getRow(nameRange.getFirstRow() + rowsPerItem + i);
-                copyRow(sourceRow, destinationRow);
-                for (int j=0; j<sheet.getNumMergedRegions(); j++) {
-                    CellRangeAddress mergedRegion = sheet.getMergedRegion(j);
-                    System.out.println(mergedRegion);
-                    if (mergedRegion.getFirstRow() != (nameRange.getFirstRow() + i)) continue;
-                    mergeCellAtSpecificRow(mergedRegion, rowsPerItem);
-                }
+            int startRowNum = nameRange.getLastRow() + 1;
+            // JSONの行データ分
+            JsonNode rows = rowNode.get(rowName).get("value");
+            // TODO don't use loop counter
+            int i = 0;
+            for (JsonNode row : rows) {
+                prepareRows(nameRange, rowsPerItem, startRowNum);
+                setValueByName(row, rowsPerItem, i);
+                i++;
             }
         }
     }
-    private void prepareNextRow(CellRangeAddress range, int rowsPerItem) {
-        // コピー元行の次の行から
-        int rowNum = range.getLastRow() + 1;
-        // 1件あたりの行数分シフト
+    private void prepareRows(CellRangeAddress nameRange, int rowsPerItem, int startRowNum) {
+        // 1件あたりの行数分空行を作成し、スタイルをコピーしていく
+        shiftRows(startRowNum, rowsPerItem);
         for (int i=0; i<rowsPerItem; i++) {
-            if (sheet.getRow(rowNum + i) != null) {
-                sheet.shiftRows(rowNum + i, sheet.getLastRowNum(), 1);
+            Row sourceRow = sheet.getRow(nameRange.getFirstRow() + i);
+            Row destinationRow = sheet.getRow(nameRange.getFirstRow() + rowsPerItem + i);
+            copyStyle(sourceRow, destinationRow);
+            // セル結合されている場合は、結合設定もコピー
+            for (int j=0; j<sheet.getNumMergedRegions(); j++) {
+                CellRangeAddress mergedRegion = sheet.getMergedRegion(j);
+                if (mergedRegion.getFirstRow() != (nameRange.getFirstRow() + i)) continue;
+                mergeCell(mergedRegion, rowsPerItem);
             }
-            sheet.createRow(rowNum + i);
+        }
+    }
+    private void shiftRows(int startRowNum, int createRowCount) {
+        for (int i=0; i<createRowCount; i++) {
+            if (sheet.getRow(startRowNum + i) != null) {
+                sheet.shiftRows(startRowNum + i, sheet.getLastRowNum(), 1);
+            }
+            sheet.createRow(startRowNum + i);
         }
     }
     private Cell getTopLeftCell(CellRangeAddress range) {
@@ -98,7 +120,6 @@ public class Form {
         return nextRow.getCell(cell.getColumnIndex());
     }
     private void iterateArrayValue(Cell cell, JsonNode nodeValue) {
-        System.out.println("This is iterateArray");
         Cell nextCell = cell;
         for (JsonNode jsonNode : nodeValue) {
             setValue(nextCell, jsonNode);
@@ -106,10 +127,23 @@ public class Form {
         }
     }
     private void setValue(Cell cell, JsonNode nodeValue) {
-        System.out.println("This is String value : " + nodeValue);
-        cell.setCellValue(nodeValue.asText());
+        cell.setCellValue(nodeValue.get("value").asText());
     }
-    private void copyRow(Row sourceRow, Row destinationRow) {
+    private void setValueByName(JsonNode row, int rowsPerItem, int index) {
+        Iterator<String> rowFields = row.fieldNames();
+        while(rowFields.hasNext()) {
+            String fieldInRow = rowFields.next();
+            Name cellName = workbook.getName(fieldInRow);
+            CellRangeAddress range = CellRangeAddress.valueOf(cellName.getRefersToFormula());
+            // 基準行から1行あたりの行数 * ループ回数分移動したセルを取得
+            Cell topLeftCell = getTopLeftCell(range);
+            int nextRowNum = topLeftCell.getRowIndex() + rowsPerItem * index;
+                Cell nextCell = sheet.getRow(nextRowNum).getCell(topLeftCell.getColumnIndex());
+            setValue(nextCell, row.get(fieldInRow).get("value"));
+            System.out.println("cell: " + range + ", " + row.get(fieldInRow) + ", " + nextRowNum);
+        }
+    }
+    private void copyStyle(Row sourceRow, Row destinationRow) {
         for (int i=0; i<sourceRow.getLastCellNum(); i++) {
             Cell sourceCell = sourceRow.getCell(i);
             Cell destinationCell = destinationRow.createCell(i);
@@ -118,19 +152,9 @@ public class Form {
             CellStyle cellStyle = workbook.createCellStyle();
             cellStyle.cloneStyleFrom(sourceCell.getCellStyle());
             destinationCell.setCellStyle(cellStyle);
-
-            // cellValue
-            switch(sourceCell.getCellType()) {
-                case NUMERIC -> {
-                    destinationCell.setCellValue(sourceCell.getNumericCellValue());
-                }
-                case STRING -> {
-                    destinationCell.setCellValue(sourceCell.getStringCellValue());
-                }
-            }
         }
     }
-    private void mergeCellAtSpecificRow(CellRangeAddress sourceRange, int rowsPerItem) {
+    private void mergeCell(CellRangeAddress sourceRange, int rowsPerItem) {
         CellRangeAddress mergeCellRangeAddress = new CellRangeAddress(
                 sourceRange.getFirstRow() + rowsPerItem,
                 sourceRange.getLastRow() + rowsPerItem,
@@ -139,9 +163,16 @@ public class Form {
         );
         sheet.addMergedRegion(mergeCellRangeAddress);
     }
-    public void writeFile() throws IOException {
-        try (FileOutputStream os = new FileOutputStream(OUT_PATH)) {
-            workbook.write(os);
-        }
+    public String writeFile() throws IOException {
+        ByteArrayOutputStream byteaOutput = new ByteArrayOutputStream();
+        Base64OutputStream base64Output = new Base64OutputStream(byteaOutput);
+        workbook.write(base64Output);
+        workbook.close();
+        byteaOutput.flush();
+        byteaOutput.close();
+        base64Output.flush();
+        base64Output.close();
+        Files.delete(Paths.get(filePath));
+        return byteaOutput.toString();
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,56 +1,35 @@
 package org.example;
 
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.core.util.JacksonFeatureSet;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.poi.ss.usermodel.*;
-import org.apache.poi.ss.util.CellRangeAddress;
-import org.apache.poi.ss.util.CellReference;
-import org.apache.poi.xssf.usermodel.XSSFName;
-import org.apache.poi.xssf.usermodel.XSSFRow;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.io.*;
+import java.net.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.CodeSource;
+import java.security.ProtectionDomain;
 
 public class Main {
-    public static void main(String[] args) throws IOException, InterruptedException {
-        System.out.println("Hello world!");
-        Form form = new Form(args[0]);
-//        form.setValues();
-        form.setRowValues();
-        form.writeFile();
-
-//// create a client
-//        var client = HttpClient.newHttpClient();
-//
-//// create a request
-//        var request = HttpRequest.newBuilder(
-//                        URI.create("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"))
-//                .header("accept", "application/json")
-//                .build();
-//
-//// use the client to send the request
-////        var response = client.send(request, new JsonBodyHandler<>(APOD.class));
-//        var response = client.send(request, HttpResponse.BodyHandlers.ofString());
-//        ObjectMapper mapper = new ObjectMapper();
-//        var json = "{\"key\": {\"value\": \"123\"}}";
-//        Map<String, Object> map = mapper.readValue(json, HashMap.class);
-//        System.out.println(map.get("key"));
-//// the response:®
-//        System.out.println(map);
+    public static void main(String[] args) throws Exception {
+        if (args.length != 3) {
+            throw new Exception("パラメータの数が違います。[0]ユーザーID, [1]ファイル名, [2]データ（json）を渡してください。");
+        }
+        Path path = getApplicationPath(Main.class).getParent().resolve("files/" + args[0] + "/" + args[1]);
+        String filePathString = path.toString() + ".xlsx";
+        if (!Files.exists(Paths.get(filePathString))) {
+            throw  new FileNotFoundException("対象のファイルが存在しません。" + filePathString);
+        }
+        String json = args[2];
+        Form form = new Form(filePathString, json);
+        form.setValues();
+//        form.setRowValues();
+        String base64 = form.writeFile();
+        System.out.println(base64);
+    }
+    public static Path getApplicationPath(Class<?> cls) throws URISyntaxException {
+        ProtectionDomain pd = cls.getProtectionDomain();
+        CodeSource cs = pd.getCodeSource();
+        URL location = cs.getLocation();
+        URI uri = location.toURI();
+        return Paths.get(uri);
     }
 }
